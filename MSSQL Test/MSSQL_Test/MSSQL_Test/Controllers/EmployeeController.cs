900ec478b08476b6ade7e467ab51925ac0d1880c
Nicholas Urrutia <battlebot626@hotmail.com>
2022-06-27 14:56:01 -0400

    Pictures of proof. Bugs found, must fix

--- MSSQL Test/MSSQL_Test/MSSQL_Test/Controllers/EmployeeController.cs
+++ MSSQL Test/MSSQL_Test/MSSQL_Test/Controllers/EmployeeController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 using MSSQL_Test.Entities;
 using System;
 
@@ -9,10 +7,8 @@ namespace EmployeeAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController : ControllerBase
+    public class EmployeeController
     {
-        //private readonly MSSQL_TestContext dbEmployee = new MSSQL_TestContext();
-
         private Business_Logic_Layer.EmployeeBL _BL;
         public EmployeeController()
         {
@@ -24,11 +20,6 @@ namespace EmployeeAPI.Controllers
         [Route("")]
         public IActionResult GetEmployees()
         {
-            //var empList = from e in dbEmployee.Employees
-            //              select e;
-
-            //return Ok(empList);
-
             return _BL.GetEmployees();
         }
         #endregion
@@ -38,25 +29,6 @@ namespace EmployeeAPI.Controllers
         [Route("{empID:int}")]
         public IActionResult SearchEmployee(int id)
         {
-            //try
-            //{
-            //    var searchResult = from e in dbEmployee.Employees
-            //                       where e.empId == id
-            //                       select e;
-            //    if (searchResult != null)
-            //    {
-            //        return Ok(searchResult);
-            //    }
-            //    else
-            //    {
-            //        return BadRequest("Could not find employee with that ID");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    return BadRequest(ex.Message);
-            //}
-
             return _BL.SearchEmployee(id);
         }
         #endregion
@@ -66,25 +38,6 @@ namespace EmployeeAPI.Controllers
         [Route("")]
         public IActionResult CreateEmployee(string fName, string lName, int age)
         {
-            //var newEmployee = new Employee();
-
-            //newEmployee.empFirstName = fName;
-            //newEmployee.empLastName = lName;
-            //newEmployee.empAge = age;
-
-
-            //if (newEmployee != null)
-            //{
-            //    dbEmployee.Employees.Add(newEmployee);
-            //    dbEmployee.SaveChanges();
-
-            //    return Created("", newEmployee);
-            //}
-            //else
-            //{
-            //    return BadRequest("Error: Could not make new Employee");
-            //}
-
             return _BL.CreateEmployee(fName, lName, age);
         }
         #endregion
@@ -94,32 +47,6 @@ namespace EmployeeAPI.Controllers
         [Route("{empID:int}")]
         public IActionResult UpdateEmployee(int id, Employee employee)
         {
-            //try
-            //{
-            //    var searchResult = (from e in dbEmployee.Employees
-            //                       where e.empId == id
-            //                       select e).SingleOrDefault();
-            //    if (searchResult != null)
-            //    {
-            //        searchResult.empFirstName = employee.empFirstName;
-            //        searchResult.empLastName = employee.empLastName;
-            //        searchResult.empAge = employee.empAge;
-
-            //        dbEmployee.SaveChanges();
-
-            //    }
-            //    else
-            //    {
-            //        return BadRequest("Could not find employee with that ID");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    return BadRequest(ex.Message);
-            //}
-
-            //return Ok("Employee updated");
-
             return _BL.UpdateEmployee(id, employee);
         }
         #endregion
@@ -129,24 +56,8 @@ namespace EmployeeAPI.Controllers
         [Route("{empID:int}")]
         public IActionResult DeleteEmployee(int empID)
         {
-            //var emp = (from e in dbEmployee.Employees
-            //           where e.empId == empID
-            //           select e).SingleOrDefault();
-
-            //if (emp != null)
-            //{
-            //    dbEmployee.Employees.Remove(emp);
-            //    dbEmployee.SaveChanges();
-            //    return Ok("Employee deleted.");
-            //}
-            //else
-            //{
-            //    return BadRequest("Invalid Employee ID.");
-            //}
-
             return _BL.DeleteEmployee(empID);
         }
         #endregion
-        
     }
 }
